07e15501a52f7de6b53c45b892385f5ba5ac549a
Tyler Nguyen <tynguyen06@gmail.com>
2017-05-22 20:02:42 -0700

    passed props to child components

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
         </div>
 
         <div className="App-intro">
-          Hello, <Name />
+          Hello, <Name name='Tyler' class='inline'/>
         </div>
 
       </div>
